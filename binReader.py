fcdf94262353c83238d0cf65b7e2b37e30c63960
WangKBJames <honghu1982@163.com>
2021-11-04 01:42:36 +0800

    modify1

--- binReader.py
+++ binReader.py
@@ -8,47 +8,48 @@ def bin_reader(file_path):
         count = 0  # 已读取字节数
         # 获取文件字符数
         count_bytes = f.seek(0, 2)
-        print(count_bytes)
+        # print(count_bytes)
         f.seek(0)
         # 文件版本号字节长度
         len_ver_str = int.from_bytes(f.read(1), byteorder='little', signed=False)
         ver_str = str(f.read(len_ver_str), encoding="utf-8")
-        print(ver_str)
+        # print(ver_str)
         # 传感器类型字节长度
         len_type_str = int.from_bytes(f.read(1), byteorder='little', signed=False)
         type_str = str(f.read(len_type_str), encoding="utf-8")
-        print(type_str)
+        # print(type_str)
         # 传感器安装位置字节长度
         len_pos_str = int.from_bytes(f.read(1), byteorder='little', signed=False)
         pos_str = str(f.read(len_pos_str))
-        print(pos_str)
+        # print(pos_str)
         # 文件存储开始时间
         id_start_time = int.from_bytes(f.read(4), byteorder='little', signed=False)
-        print(id_start_time)
+        # print(id_start_time)
         # 传感器编号字节长度
         len_sensor_num = int.from_bytes(f.read(1), byteorder='little', signed=False)
-        print(len_sensor_num)
+        # print(len_sensor_num)
         # 传感器编号
         sensor_num = str(f.read(len_sensor_num), encoding="utf-8")
-        print(sensor_num)
+        # print(sensor_num)
         # 采样频率
         sample_frq = struct.unpack('f', f.read(4))[0]
-        print(sample_frq)
+        # print(sample_frq)
         # 采样精度
         sample_precision = struct.unpack('f', f.read(4))[0]
-        print(sample_precision)
+        # print(sample_precision)
         # 放大倍数
         scalor = int.from_bytes(f.read(4), byteorder='little', signed=False)
-        print(scalor)
+        # print(scalor)
         # 灵敏度
         sensitivity = struct.unpack('f', f.read(4))[0]
-        print(sensitivity)
+        # print(sensitivity)
         # 字符'$', 表示文件头的终结
-        print(str(f.read(1), encoding="utf-8"))
+        f.read(1)
+        # print(str(f.read(1), encoding="utf-8"))
         data = list()
         # 数据数量
         count_data = int((count_bytes - f.seek(0, 1)) / 4)
-        print(count_data)
+        # print(count_data)
         data = [struct.unpack('f', f.read(4))[0] for i in range(count_data)]
         # print(data)
         f.close()
--- dataReader.py
+++ dataReader.py
@@ -2,6 +2,7 @@ import datetime
 import glob
 from binReader import bin_reader
 from timeParser import time_parser, time_list
+import calendar
 
 
 def data_reader(main_path, sensor_num, t_start_list, t_end_list, sample_frq):
@@ -9,36 +10,92 @@ def data_reader(main_path, sensor_num, t_start_list, t_end_list, sample_frq):
     t_end = datetime.datetime(*t_end_list)
     t_delta = t_end - t_start
     s_delta = t_delta.seconds
+    data = []
+    t_list = []
     if s_delta < 0:
         print('=======> 查询结速时间早于开始时间，查询异常！')
-        return [], []
-    if t_start.day == t_end.day:
+        return t_list, data
+    if (t_start.day == t_end.day) & (t_start.month == t_end.month) & (t_start.year == t_end.year):
         if t_start.hour == t_end.hour:
             t = t_start_list[0:4]
             file_list = data_location(main_path, sensor_num, t)
-            data = bin_reader(file_list[0])
+            if len(file_list) > 0:
+                data = bin_reader(file_list[0])
             if len(data) > 0:
                 t_datetime = time_parser(file_list[0])
                 t_list = time_list(t_datetime, len(data), sample_frq)
-            else:
-                t_list = []
         else:
-            data = []
-            t_list = []
+
             for i in range(t_start.hour, t_end.hour + 1):
                 ti = t_start.replace(hour=i)
                 t = [ti.year, ti.month, ti.day, ti.hour]
                 file_list = data_location(main_path, sensor_num, t)
-                data_i = bin_reader(file_list[0])
+                if len(file_list) > 0:
+                    data_i = bin_reader(file_list[0])
                 if len(data_i) > 0:
                     data.extend(data_i)
                     t_datetime = time_parser(file_list[0])
-                    # print(t_datetime)
                     t_list_i = time_list(t_datetime, len(data_i), sample_frq)
-                    # print(len(data))
                     t_list.extend(t_list_i)
     else:
-        pass
+        for i in range(t_start.year, t_end.year + 1):
+            if i == t_start.year:
+                month_start_num = t_start.month
+                if i == t_end.year:
+                    month_end_num = t_end.month
+                else:
+                    month_end_num = int(12)
+            elif i == t_end.year:
+                month_end_num = t_end.month
+                if i == t_start.year:
+                    month_start_num = t_start.month
+                else:
+                    month_start_num = int(1)
+            else:
+                month_start_num = int(1)
+                month_end_num = int(12)
+            for j in range(month_start_num, month_end_num + 1):
+                if (j == t_start.month) & (i == t_start.year):
+                    day_start_num = t_start.day
+                    if (j == t_end.month) & (i == t_end.year):
+                        day_end_num = t_end.day
+                    else:
+                        day_end_num = calendar.monthrange(i, j)[1]
+                elif (j == t_end.month) & (i == t_end.year):
+                    day_end_num = t_end.day
+                    if (j == t_start.month) & (i == t_start.year):
+                        day_start_num = t_start.day
+                    else:
+                        day_start_num = int(1)
+                else:
+                    day_start_num = int(1)
+                    day_end_num = calendar.monthrange(i, j)[1]
+                    for k in range(day_start_num, day_end_num + 1):
+                        if (k == t_start.day) & (j == t_start.month) & (i == t_start.year):
+                            h_start_num = t_start.hour
+                            h_end_num = int(23)
+                        elif (k == t_end.day) & (j == t_end.month) & (i == t_end.year):
+                            h_start_num = int(0)
+                            h_end_num = t_end.hour
+                        else:
+                            h_start_num = int(0)
+                            h_end_num = int(23)
+                        if (h_start_num == 0) & (h_end_num == 23):
+                            t = [i, j, k]
+                            file_list = data_location(main_path, sensor_num, t)
+                        else:
+                            file_list = []
+                            for h in range(h_start_num, h_end_num + 1):
+                                t = [i, j, k, h]
+                                file_list.extend(data_location(main_path, sensor_num, t))
+                        if len(file_list) > 0:
+                            for file_str in file_list:
+                                data_i = bin_reader(file_str)
+                                if len(data_i) > 0:
+                                    data.extend(data_i)
+                                    t_datetime = time_parser(file_list[0])
+                                    t_list_i = time_list(t_datetime, len(data_i), sample_frq)
+                                    t_list.extend(t_list_i)
 
     return t_list, data
 
