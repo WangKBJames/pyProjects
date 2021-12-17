a9621c05cb8df6594dec020f29ba1ff257604c46
WangKBJames <honghu1982@163.com>
2021-12-17 16:44:54 +0800

    test2

--- pythonProject/modal_Plot.py
+++ pythonProject/modal_Plot.py
@@ -2,8 +2,7 @@ from mpl_toolkits.mplot3d import axes3d
 import numpy as np
 import matplotlib.pyplot as plt
 
-# x = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-x = range(0, 9)
+x = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
 y = [-1, 1]
 z = [[0, 0.7, 1, 0.72, 0, -0.69, -1, -0.67, 0], [0, 0.7, 1, 0.72, 0, -0.69, -1, -0.67, 0]]
 z = np.array(z)
@@ -11,14 +10,13 @@ x, y = np.meshgrid(x, y)
 fig = plt.figure(figsize=(9, 3), dpi=80)
 ax = fig.add_subplot(111, projection='3d')
 # ax = fig.add_axes([0.15, 0.1, 0.7, 0.3],projection='3d')
-ax.set_position([0.05, 0.05, 0.85, 0.85])
+ax.set_position([0.05, 0.05, 0.9, 0.9])
 ax.set_xticks(np.linspace(0, 1, 9))
-
-# axes3d.Axes.set_position(ax, pos=[0.15, 0.1, 0.7, 0.3])
-# Grab some test data.
-# X, Y, Z = axes3d.get_test_data(0.05)
+ax.set_xlim([0,1])
+ax.set_ylim([-1,1])
+# ax.xlim()
 
 # Plot a basic wireframe.
-ax.plot_wireframe(x, y, z, rstride=9, cstride=2, color='#dc4105')
-ax.view_init(45,45)
+ax.plot_wireframe(x, y, z, rstride=10, cstride=2, color='#dc4105')
+ax.view_init(45,250)
 plt.show()
