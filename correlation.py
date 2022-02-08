c74a887135607c9a720aa811c2b1d2acf8524d36
WangKBJames <honghu1982@163.com>
2022-02-09 02:33:35 +0800

    modify1

--- correlation.py
+++ correlation.py
@@ -1,43 +1,13 @@
-import numpy as np
-from scipy import signal
-import matplotlib.pyplot as plt
-
-'''
-
-计算相关函数
-输入：
-x_signal：float[]，时程数据1，多为加速度
-y_signal：float[]，时程数据2，多为加速度
-sample_frequency： float，数据x_signal、y_signal的采样频率，如加速度为20或50Hz
-输出：
-x_fit: float[]，频率，单位：Hz
-y_fit：float[]，相干函数
-a: float，相关函y=a*x+b,系数k
-b: float，相关函y=a*x+b,a
-corr：float，相关系数
-
-'''
-
-
-def correlation(x_signal, y_signal):
-    if type(x_signal) is not np.ndarray:
-        x_signal = np.array(x_signal, dtype='float')
-    if type(x_signal) is not np.ndarray:
-        y_signal = np.array(y_signal, dtype='float')
-
-    frq, cxy = signal.coherence(x_signal, y_signal, sample_frequency, nperseg=1024)
-    return frq.tolist(), cxy.tolist(), np.float(cxy.max()), np.float(cxy.min()), np.float(cxy.mean())
-
-
 # -*- coding: utf-8 -*-
 """
-@author: 蔚蓝的天空TOM
-Talk is cheap, show me the code
-Aim：最小二乘法库函数leastsq使用示例详解
+Aim：两变量线形拟合
 """
 import numpy as np
-import matplotlib.pyplot as plt
+# from scipy import signal
 from scipy.optimize import leastsq
+# from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 
 
 # hypothesis function
@@ -47,8 +17,7 @@ def hypothesis_func(w, x):
 
 
 # error function
-def error_func(w, train_x, train_y, msg):
-    print(msg)
+def error_func(w, train_x, train_y):
     return hypothesis_func(w, train_x) - train_y
 
 
@@ -65,54 +34,48 @@ def dump_fit_cost(w_fit, train_x, train_y):
     print('fitting cost:', str(square_error))
     return square_error
 
+'''
 
-# main function
+计算相关函数
+输入：
+x_signal：float[]，时程数据1
+y_signal：float[]，时程数据2
+输出：
+x_fit: float[]，频率，单位：Hz
+y_fit：float[]，相干函数
+a: float，相关函y=a*x+b,系数a
+b: float，相关函y=a*x+b,a
+corr：float，相关系数
+
+'''
+
+
+def correlation(x_signal, y_signal):
+    if type(x_signal) is not np.ndarray:
+        x_signal = np.array(x_signal, dtype='float')
+    if type(x_signal) is not np.ndarray:
+        y_signal = np.array(y_signal, dtype='float')
+    w_init = [1.0, 1.0]
+    fit_ret = leastsq(error_func, w_init, args=(x_signal, y_signal))
+    a, b = fit_ret[0]
+    x_fit = x_signal
+    y_fit = hypothesis_func([a, b], x_fit)
+    corr = pearsonr(x_signal, y_signal)
+    # return x_fit.tolist(), y_fit.tolist(), np.float(a), np.float(b), np.float(corr[0])
+    return x_fit.tolist(), y_fit.tolist(), float(a), float(b), float(corr[0])
+
+
 if __name__ == "__main__":
     # train set
-    train_x = np.array([8.19, 2.72, 6.39, 8.71, 4.7, 2.66, 3.78])
-    train_y = np.array([7.01, 2.78, 6.47, 6.71, 4.1, 4.23, 4.05])
-
-    # linear regression by leastsq
-    msg = "invoke scipy leastsq"
-    w_init = [20, 1]  # weight factor init
-    fit_ret = leastsq(error_func, w_init, args=(train_x, train_y, msg))
-    w_fit = fit_ret[0]
-
-    # dump fit result
-    dump_fit_func(w_fit)
-    fit_cost = dump_fit_cost(w_fit, train_x, train_y)
-
-    # test set
-    test_x = np.array(np.arange(train_x.min(), train_x.max(), 1.0))
-    test_y = hypothesis_func(w_fit, test_x)
+    x_signal = np.array([8.19, 2.72, 6.39, 8.71, 4.7, 2.66, 3.78])
+    y_signal = np.array([7.01, 2.78, 6.47, 6.71, 4.1, 4.23, 4.05])*0.01
+    x_fit, y_fit, a, b, corr = correlation(x_signal, y_signal)
 
     # show result by figure
-    plt.figure(1)
+    # plt.figure(1)
     plt.figure(figsize=(8, 6))  # 指定图像比例： 8：6
     plt.title('linear regression by scipy leastsq')
-    plt.scatter(train_x, train_y, color='b', label='train set')
-    plt.scatter(test_x, test_y, color='r', marker='^', label='test set', linewidth=2)
-    plt.plot(test_x, test_y, color='r', label='fitting line')
+    plt.scatter(x_signal, y_signal, color='b', label='train set')
+    plt.plot(x_fit, y_fit, color='r', label='fitting line')
     plt.legend(loc='lower right')  # label面板放到figure的右下角
-
     plt.show()
-
-
-# if __name__ == "__main__":
-#     fs = 10e3
-#     N = 1e5
-#     amp = 20
-#     freq = 1234.0
-#     noise_power = 0.001 * fs / 2
-#     time = np.arange(N) / fs
-#     b, a = signal.butter(2, 0.25, 'low')
-#     x = np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
-#     y = signal.lfilter(b, a, x)
-#     x += amp * np.sin(2 * np.pi * freq * time)
-#     y += np.random.normal(scale=0.1 * np.sqrt(noise_power), size=time.shape)
-#     frq, cxy, cxymax, cxymin, cxymean = coherence(x, y, fs)
-#     # f, Cxy = signal.coherence(x, y, fs, nperseg=1024)
-#     plt.semilogy(f.tolist(), Cxy.tolist())
-#     plt.xlabel('frequency [Hz]')
-#     plt.ylabel('Coherence')
-#     plt.show()
---
+++ feature_extraction.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Aim：频谱图、累积量(速度)提取
+"""
+import numpy as np
+from scipy.fftpack import fft
+from scipy.optimize import leastsq
+import matplotlib.pyplot as plt
+from scipy.optimize import leastsq
+from scipy.stats import pearsonr
+
+
+def spectrum(y_signal, fs):
+    if type(y_signal) is not np.ndarray:
+        y_signal = np.array(y_signal, dtype='float')
+    fft_y = np.fft.rfft(y_signal)
+    n = int(len(y_signal))
+    abs_fy = np.abs(fft_y) / n * 2
+    freqs = np.linspace(0, fs / 2, int(n / 2 + 1))
+    frqp = freqs[np.argmax(abs_fy)]
+    return freqs.tolist(), abs_fy.tolist(), float(frqp)
+
+
+# hypothesis function
+def hypothesis_func(w, x):
+    w1, w0 = w
+    return w1 * x + w0
+
+
+# error function
+def error_func(w, train_x, train_y):
+    return hypothesis_func(w, train_x) - train_y
+
+
+def cumulation(y_signal, fs):
+    if type(y_signal) is not np.ndarray:
+        y_signal = np.array(y_signal, dtype='float')
+    y_cum = np.array([0], dtype='float') + np.cumsum(np.abs(np.diff(y_signal)))
+    dt = 1 / float(fs)
+    t = np.arange(start=0.0, stop=len(y_cum) * dt, step=dt, dtype='float')
+    w_init = [1.0, 1.0]
+    fit_ret = leastsq(error_func, w_init, args=(t, y_cum))
+    a, b = fit_ret[0]
+    y_fit = hypothesis_func([a, b], t)
+    corr = pearsonr(y_fit, y_cum)
+    return y_cum.tolist(), y_fit.tolist(), float(a), float(b), float(corr[0])
+
+
+if __name__ == "__main__":
+    # train set
+    y_signal = np.array([-1.2, 1.2, -1.2, 1.2, -1.2, 1.2, -1.2, 1.2, -1.2, 1.2, -1.2, 1.2]) * 0.01
+    fs = 1.0
+    f, fy, frqp = spectrum(y_signal, fs)
+    plt.figure(figsize=(8, 6))  # 指定图像比例： 8：6
+    plt.title('frequency spectrum')
+    # plt.scatter(x_signal, y_signal, color='b', label='train set')
+    plt.plot(f, fy, color='r', label='spectrum')
+    plt.legend(loc='lower right')  # label面板放到figure的右下角
+    plt.show()
+
+    y_signal = np.random.random(30)-0.5
+    fs = 1
+    y_cum, y_fit, a, b, corr = cumulation(y_signal, fs)
+    plt.figure(figsize=(8, 6))  # 指定图像比例： 8：6
+    plt.title('cumsum')
+    # plt.scatter(x_signal, y_signal, color='b', label='train set')
+    plt.plot(y_cum, color='r', label='cumsum')
+    plt.plot(y_fit, color='b', label='cumfit')
+    plt.legend(loc='lower right')  # label面板放到figure的右下角
+    plt.show()
