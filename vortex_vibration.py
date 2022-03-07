db38b9bbdd19b39a79bfbb0981f685f368ea59f2
WangKBJames <honghu1982@163.com>
2022-03-08 01:01:31 +0800

    modify1

---
+++ vortex_vibration.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import math
+
+main_path = r".\vertex_vibration\\"
+
+
+def vv_probability(acc_data, frq=50):
+    pass
+
+
+
+
+def movstd(y_signal, win_n):
+    '''
+
+    :param y_signal:
+    :param win_n:
+    :return:
+    '''
+    y_std = np.array([])
+    for i in range(0, len(y_signal)):
+        if i < int(win_n // 2):
+            y_std = np.append(y_std, np.nanstd(y_signal[:i + int(math.ceil(int(win_n) / 2))]))
+        elif i >= len(y_signal) - int(math.floor(win_n / 2)):
+            y_std = np.append(y_std, np.nanstd(y_signal[i - int(math.floor(win_n / 2)):]))
+        else:
+            y_std = np.append(y_std, np.nanstd(y_signal[i - math.floor(win_n / 2):i + int(math.ceil(win_n / 2))]))
+    return y_std.tolist()
+
+
+def power_spectrum(data, frq):
+    pass
+
+
+def find_peaks(data):
+    pass
+
+
