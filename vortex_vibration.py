b4d5ed2fbcb84db17188f5f1842ab14248207055
WangKBJames <honghu1982@163.com>
2022-03-10 01:23:30 +0800

    modify1

--- vortex_vibration.py
+++ vortex_vibration.py
@@ -37,3 +37,11 @@ def find_peaks(data):
     pass
 
 
+if __name__ == "__main__":
+    from scipy import signal
+    import numpy as np
+
+    xs = np.arange(0, np.pi, 0.05)
+    data = np.sin(xs)
+    peakind = signal.find_peaks_cwt(data, np.arange(1, 10))
+    peakind, xs[peakind], data[peakind]
